ee6264f0fb3b61c4830664fb74fced0ad6980b70
MockTurtle <aksenov005@yandex.ru>
2024-07-12 12:25:14 +0400

    Initial commit

--- backend.py
+++ backend.py
@@ -1,86 +1,101 @@
+import pandas
 
 
-def convert_cl(cl):
-    """ Returns milliliters"""
-    return int(cl * 10)
+def convert(cl: int | float) -> int:  # convert cl to ml
+    ml = cl * 10
+    return int(ml)
 
 
-def get_ingr_list(recipe_book):
-    ingr_list = []
-    for cocktail in recipe_book:
-        for item in cocktail['ingredients']:
-            try:
-                if item['ingredient'] not in ingr_list:
-                    ingr_list.append(item['ingredient'])
-            except KeyError:
-                pass
-    return sorted(ingr_list)
-
-
-def get_name_list(ingredient, recipe_book):
-    name_list = []
-    if not ingredient:
-        for cocktail in recipe_book:
-            name_list.append(cocktail['name'])
-        return sorted(name_list)
-    else:
-        found_matches = []
-        for cocktail in recipe_book:
-            for item in cocktail['ingredients']:
+def get_name_list(recipe_book: pandas.DataFrame, ingredient: str) -> list[str]:  # get names of cocktails from dataframe
+    if ingredient:  # show only cocktails containing specific ingredient
+        name_list = []
+        for index, recipe in recipe_book.iterrows():
+            for item in recipe['ingredients']:
                 try:
                     if ingredient in item['ingredient']:
-                        found_matches.append(cocktail['name'])
+                        name_list.append(recipe['name'])
                 except KeyError:
                     continue
-        return sorted(found_matches)
+    else:  # show all cocktails in the database
+        name_list = recipe_book['name'].values
+
+    return sorted(name_list)
 
 
+def get_ingredients_list(recipe_book: pandas.DataFrame) -> list[str]:  # get ingredients of all cocktails from dataframe
+    ingredients_list = []
+    for index, recipe in recipe_book.iterrows():
+        for item in recipe['ingredients']:
+            try:
+                if item['ingredient'] not in ingredients_list:
+                    ingredients_list.append(item['ingredient'])
+            except KeyError:
+                pass
+    return sorted(ingredients_list)
 
 
-def display_info(name, recipe_book):
-    info = []
-    for cocktail in recipe_book:
-        if cocktail['name'] == name:
-            article = "an" if cocktail['glass'].startswith("o") else "a"
-            info.append(f"{cocktail['name']} is typically served in {article} "
-                        f"{cocktail['glass']} glass.\n\n"
-                        f"Ingredients:\n")
-            for item in cocktail["ingredients"]:
+class Cocktail:
+    def __init__(self, name, df):
+        self.name = name
+        self.row_index = df.index[df['name'] == name].tolist()
+        self.ingredient_list = df.iloc[self.row_index]['ingredients'].squeeze()
+        self.garnish = df.iloc[self.row_index]['garnish'].squeeze()
+        self.glass = df.iloc[self.row_index]['glass'].squeeze()
+        self.preparation = df.iloc[self.row_index]['preparation'].squeeze()
+
+    def extract_ingredient_list(self) -> str:  # extract ingredient info from cocktail and format it
+        ingredients: str = 'Ingredients:\n\n'
+        for item in self.ingredient_list:
+            try:
+                ingredients += f"- {convert(item['amount'])}ml of {item['ingredient']} ({item['label']})\n\n"
+            except KeyError:
                 try:
-                    info.append(f"{convert_cl(item['amount'])}ml of {item['ingredient']} ({item['label']})\n")
-                except KeyError:
-                    try:
-                        info.append(f"{convert_cl(item['amount'])}ml of {item['ingredient']}\n")
-                    except KeyError:
-                        pass
-            info.append('\n')
-            for item in cocktail["ingredients"]:
-                try:
-                    info.append(item['special'] + '\n')
+                    ingredients += f"- {convert(item['amount'])}ml of {item['ingredient']}\n\n"
                 except KeyError:
                     pass
-            info.append('\n')
             try:
-                info.append(f"\nUsually garnished with {cocktail['garnish'].lower()}\n")
+                ingredients += f"- {item['special']}\n\n"
             except KeyError:
                 pass
 
-            try:
-                info.append(f"Preparation:\n")
-                info.append(f"{cocktail['preparation']}")
-            except KeyError:
-                pass
+        return ingredients
 
-    return info
+    def extract_garnish(self) -> str:  # extract garnish info from cocktail and format it
+        if self.garnish == self.garnish:  # check if value is not NaN
+            garnish: str = f"\nUsually garnished with {self.garnish.lower()}\n"
+            return garnish
+        else:
+            return ""
 
+    def extract_glass(self) -> str:  # extract glass info from cocktail and format it
+        if self.glass == self.glass:  # check if value is not NaN
+            article: str = "an" if self.glass.startswith("o") else "a"
+            glass: str = f"{self.name} is typically served in {article} {self.glass} glass.\n\n"
+            return glass
+        else:
+            return ""
 
-def search_by_ingredient(ingredient, recipe_book):
-    found_matches = []
-    for cocktail in recipe_book:
-        for item in cocktail['ingredients']:
-            try:
-                if ingredient in item['ingredient']:
-                    found_matches.append(cocktail['name'])
-            except KeyError:
-                continue
-    return sorted(found_matches)
+    def extract_preparation(self) -> str:  # extract preparation info from cocktail and format it
+        if self.preparation == self.preparation:  # check if value is not NaN
+            preparation = "Preparation:\n\n" + self.preparation
+            return preparation
+        else:
+            return ""
+
+    def print_recipe(self, glass=True, ingredient_list=True, garnish=True, preparation=True) -> str:
+        info_list: list[str] = []
+
+        if glass:
+            info_list.append(self.extract_glass())
+
+        if ingredient_list:
+            info_list.append(self.extract_ingredient_list())
+
+        if garnish:
+            info_list.append(self.extract_garnish())
+
+        if preparation:
+            info_list.append(self.extract_preparation())
+
+        info_str: str = '\n'.join(info_list)
+        return info_str
--- main.py
+++ main.py
@@ -1,103 +1,8 @@
 import pandas
 import streamlit as st
+import backend
 
-df = pandas.read_json('recipes.json')
-
-
-def convert(cl: int) -> int:
-    ml = cl * 10
-    return int(ml)
-
-
-def get_name_list(recipe_book, ingredient: str) -> list[str]:
-    if not ingredient:
-        name_list = recipe_book['name'].values
-        return sorted(name_list)
-    else:
-        found_matches = []
-        for index, recipe in recipe_book.iterrows():
-            for item in recipe['ingredients']:
-                try:
-                    if ingredient in item['ingredient']:
-                        found_matches.append(recipe['name'])
-                except KeyError:
-                    continue
-        return sorted(found_matches)
-
-
-def get_ingredients_list(recipe_book) -> list[str]:
-    ingr_list = []
-    for index, recipe in recipe_book.iterrows():
-        for item in recipe['ingredients']:
-            try:
-                if item['ingredient'] not in ingr_list:
-                    ingr_list.append(item['ingredient'])
-            except KeyError:
-                pass
-    return sorted(ingr_list)
-
-
-class Cocktail:
-    def __init__(self, name):
-        self.name = name
-        self.row_index = df.index[df['name'] == name].tolist()
-        self.ingredient_list = df.iloc[self.row_index]['ingredients'].squeeze()
-        self.garnish = df.iloc[self.row_index]['garnish'].squeeze()
-        self.glass = df.iloc[self.row_index]['glass'].squeeze()
-        self.preparation = df.iloc[self.row_index]['preparation'].squeeze()
-
-    def extract_ingredient_list(self) -> str:
-        ingredients: str = "Ingredients:\n"
-        for item in self.ingredient_list:
-            try:
-                ingredients += f"{convert(item['amount'])}ml of {item['ingredient']} ({item['label']})\n"
-            except KeyError:
-                try:
-                    ingredients += f"{convert(item['amount'])}ml of {item['ingredient']}\n"
-                except KeyError:
-                    pass
-        ingredients += '\n'
-        return ingredients
-
-    def extract_garnish(self) -> str:
-        if self.garnish:
-            garnish: str = f"\nUsually garnished with {self.garnish.lower()}\n"
-            return garnish
-        else:
-            pass
-
-    def extract_glass(self) -> str:
-        if self.glass:
-            article: str = "an" if self.glass.startswith("o") else "a"
-            glass: str = f"{self.name} is typically served in {article} {self.glass} glass.\n\n"
-            return glass
-        else:
-            pass
-
-    def extract_preparation(self) -> str:
-        if self.preparation:
-            preparation = "Preparation:\n\n" + self.preparation
-            return preparation
-        else:
-            pass
-
-    def print_recipe(self, glass=True, ingredient_list=True, garnish=True, preparation=True) -> str:
-        info_list = []
-
-        if glass:
-            info_list.append(self.extract_glass())
-
-        if ingredient_list:
-            info_list.append(self.extract_ingredient_list())
-
-        if garnish:
-            info_list.append(self.extract_garnish())
-
-        if preparation:
-            info_list.append(self.extract_preparation())
-
-        info_str: str = '\n'.join(info_list)
-        return info_str
+df = pandas.read_json('recipes.json')  # load recipe book
 
 
 # page layout
@@ -107,10 +12,11 @@ st.header('Cocktail Database')
 
 col1, col2 = st.columns([0.5, 1.5])
 with col1:
-    ingr_filter = st.selectbox("Search by ingredient", get_ingredients_list(df), None)
+    ingredient_filter = st.selectbox("Search by ingredient", backend.get_ingredients_list(df), None)
     name_select = st.selectbox('Available recipes:',
-                               get_name_list(df, ingr_filter), None)
-    cocktail = Cocktail(name_select)
+                               backend.get_name_list(df, ingredient_filter), None)
+
+    cocktail = backend.Cocktail(name_select, df)
 
 with col2:
     if name_select:
